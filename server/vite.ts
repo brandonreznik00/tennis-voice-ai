1663b6dbb657535daf3d207026436056fed94e07
brandonreznik00 <brandonreznik00@gmail.com>
2025-10-15 10:22:15 -0400

    Update vite.ts

--- server/vite.ts
+++ server/vite.ts
@@ -71,9 +71,7 @@ export function serveStatic(app: Express) {
   const distPath = path.resolve(import.meta.dirname, "public");
 
   if (!fs.existsSync(distPath)) {
-    throw new Error(
-      `Could not find the build directory: ${distPath}, make sure to build the client first`,
-    );
+   
   }
 
   app.use(express.static(distPath));
